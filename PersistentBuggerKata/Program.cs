6b1d01e7812af737d19f2bf23e4939cc4860ec77
Pierre <pierrevdw93@gmail.com>
2021-01-13 21:22:50 +0200

    initial commit

---
+++ PersistentBuggerKata/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace PersistentBuggerKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = 39;
+
+            var numbers = num.ToString().ToList().ConvertAll(x => x.ToString());
+            var canMultiply = numbers.Count > 1;
+            var value = 0;
+
+            while(canMultiply)
+            {
+                if (numbers.Count == 1)
+                    break;
+
+                var total = Convert.ToInt32(numbers[0]);
+
+                for (int i = 1; i < numbers.Count; i++)
+                {
+                    total *= Convert.ToInt32(numbers[i]);
+                }
+
+                value++;
+                numbers = total.ToString().ToList().ConvertAll(x => x.ToString());
+            }
+        }
+    }
+}
